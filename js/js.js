760c6b68c697f36f6522610286dc3d80a06e1dc6
Thierry NGUYEN <Thierry NGUYEN>
2016-03-03 10:02:21 +0100

    test

--- js/js.js
+++ js/js.js
@@ -46,18 +46,38 @@ function logo(logos,place){
     img.attr("src",logos[i]);
     img.css({
       "width":"100%",
-      "height":"100%"
+      "height":"100%",
+      "margin":"1em 0"
     });
     div.css({
-      "background-color": "rgb(48, 60, 86)",
-      // "width":"100px",
-      // "height":"auto",
-      "flex-grow":"0"
+      "background-color": "rgb(48, 60, 86)"
     });
+    img.attr("data-id",i);
     div.append(img);
     $(place).append(div);
   }
 }
+var idImg;
+$(".connaitre").on("mouseover","img",function(){
+ idImg=$(this).attr("src");
+  $(this).attr("src","red-"+idImg);
+});
+
+$(".connaitre").on("mouseout","img",function(){
+  $(this).attr("src",idImg);
+});
+
+$("nav").on("click","a",function(e){
+  var target = $(this).attr('href');
+  e.preventDefault();
+  $('html, body').stop().animate({scrollTop: $(target).offset().top}, 1000 );
+  // console.log("ok");
+});
+
+$(".btn_menu").click(function(){
+  $(this).fadeOut();
+  $(".menu").fadeIn();
+});
 
 logo(logo_territoire,".territoire");
 logo(acteurs_logo,".acteurs");
